9d5ae79cdebd5da5f634811c4cd987f025decb98
Runa <98182285+RunaYomozuni@users.noreply.github.com>
2022-05-30 00:46:52 +0200

    Add files via upload
    avancement sur le pokemon tamagotchi

--- lecon/JS/Les objets/Tamagotchi pokemon/assets/js/scriptMain.js
+++ lecon/JS/Les objets/Tamagotchi pokemon/assets/js/scriptMain.js
@@ -1,9 +1,9 @@
 let ball = document.querySelectorAll(".open");
-let loading_screen;
+
 
 for (let i = 0; i < ball.length; i++){
     ball[i].addEventListener("click", function (){
-        // random page
+        // random pokemon
         let pokemon = ["view/dedenne.html","view/gengar.html","view/pikachu.html","view/evoli.html"];
         let rdm = Math.floor(Math.random()*4);
         this.querySelector('img').setAttribute('src',"assets/img/pokeOpen.png");
