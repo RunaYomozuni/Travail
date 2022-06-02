28068c5a98a2b6aa4fa2018f0638a797c12b0de2
RunaYomozuni <jonascompper2@gmail.com>
2022-06-02 17:32:11 +0200

    js tamagotchi

---
+++ lecon/JS/Les objets/Tamagotchi pokemon/assets/js/gengar.js
@@ -0,0 +1,30 @@
+let gengar = document.getElementById("img");
+let fight = document.querySelector('#fight');
+let play = document.querySelector('#play');
+
+
+
+ function fighting (){
+     let gifsFight =['fight','fight2','fight3','fight4'];
+     let random = Math.floor(Math.random()*4);
+    gengar.classList.add(gifsFight[random]);
+    gengar.classList.remove('play')
+    setTimeout(function (){
+        gengar.classList.remove(gifsFight[random]);
+    },4400)
+}
+
+
+function playing (){
+    let gifsPlaying =['play','play2','play3'];
+    let random = Math.floor(Math.random()*3);
+    gengar.classList.add(gifsPlaying[random]);
+    gengar.classList.remove('fight')
+    setTimeout(function (){
+        gengar.classList.remove(gifsPlaying[random])
+    },3000)
+}
+
+
+play.addEventListener('click',playing);
+fight.addEventListener("click",fighting);
